653ec8f7c97fa98d3180d12130e4d5c15df81f09
Ttae27 <ponghong.2744@gmail.com>
2025-05-26 09:12:54 +0700

    refactor google_drive.py

--- google_drive.py
+++ google_drive.py
@@ -1,5 +1,3 @@
-import os
-import base64
 import io
 from google.oauth2 import service_account
 from googleapiclient.http import MediaIoBaseUpload
@@ -13,9 +11,6 @@ SERVICE_ACCOUNT_FILE = 'line-chat-bot-softnix-ff77b1c1d463.json'
 credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
 service = build('drive', 'v3', credentials=credentials)
-
-def get_file_id():
-    pass
 
 def to_bytes(path): 
     with open(path, 'rb') as file: 
@@ -36,7 +31,6 @@ def upload_file_drive(file: bytearray, file_name: str, about: str):
             "description": about
         }
         mimetype = 'application/' + file_name.split('.')[1]
-        # print(mimetype)
         media = MediaIoBaseUpload(io.BytesIO(file), mimetype)
         
         file = (
@@ -44,7 +38,6 @@ def upload_file_drive(file: bytearray, file_name: str, about: str):
             .create(body=file_metadata, media_body=media, fields="id")
             .execute()
         )
-        # print(f'File ID: {file.get("id")}')
 
     except HttpError as error:
         print(f"An error occurred: {error}")
@@ -60,8 +53,8 @@ def sharing_file_google(file_id: str):
         file_id: string of file ID
     """
     request_body = {
-    'role': 'reader',
-    'type': 'anyone'
+        'role': 'reader',
+        'type': 'anyone'
     }
 
     service.permissions().create(
@@ -76,14 +69,13 @@ def sharing_file_google(file_id: str):
     return response_share_link ['webViewLink']
 
 @tool
-def show_files_tool(parent_folder_id=None):
+def show_files_tool():
     """
         โชว์ไฟล์ใน google drive
 
         return id name mimeType createdTime and description
     """
     results = service.files().list(
-        # q=f"'{parent_folder_id}' in parents and trashed=false" if parent_folder_id else None,
         q=None,
         pageSize=1000,
         fields="nextPageToken, files(id, name, mimeType, createdTime, description)"
@@ -104,9 +96,8 @@ def delete_file_google(file_id):
 
     return 'delete successfully'
 
-def show_files(parent_folder_id=None):
+def show_files():
     results = service.files().list(
-        # q=f"'{parent_folder_id}' in parents and trashed=false" if parent_folder_id else None,
         q=None,
         pageSize=1000,
         fields="nextPageToken, files(id, name, mimeType, createdTime, description)"
@@ -114,14 +105,3 @@ def show_files(parent_folder_id=None):
     items = results.get('files', [])
 
     return items
-
-def update_file():
-    pass
-
-# sharing_file('1HlhOqPjWv5oEx82YZHmNkFaoVWJG5Xkg')
-# a = show_files()
-# print(a)
-
-# upload_file(to_bytes('received_files/Security Law.docx'), 'Security Law.docx', 'test')
-
-# delete_file('1JaNU5rwWEKdjx43xOPQjP3t0n-PGss2K')
